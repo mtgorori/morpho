c231ca849ec31cff569e20baf05a7e3c45435f25
mtgorori <karu7otit2heki39@gmail.com>
2018-04-16 17:44:51 +0900

    計算時間がかかってしまうことがわかった

--- challengeProblemA.py
+++ challengeProblemA.py
@@ -10,13 +10,16 @@ def main():
   #input N
   print('input N in the range 0<=N<=30')
   N = int(input())
-  N = 4**N
+  if N>30:
+      pass
+  else:
+      N = 4**N
   #list01 = range(1,N)
-  list01 = [ list(str(NsinFrom10sin(x,4))) for x in range(1,N) ]
-  #print(list01)
-  list01 = reduce(lambda x, y: x + y, list01)
-  #print(list01)
-  print(list01.count('3'))
+      list01 = [ list(str(NsinFrom10sin(x,4))) for x in range(1,N) ]
+      print(list01)
+      list01 = reduce(lambda x, y: x + y, list01)
+      print(list01)
+      print(list01.count('3'))
 
 if __name__ == "__main__":
   main()
