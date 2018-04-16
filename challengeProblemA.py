2bc7747195794f9f1eb9f5889b1101904a41dcaf
mtgorori <karu7otit2heki39@gmail.com>
2018-04-16 18:43:22 +0900

    完成？遅いけどね

--- challengeProblemA.py
+++ challengeProblemA.py
@@ -1,25 +1,24 @@
-import sys
-from functools import reduce
-
 def NsinFrom10sin(X, n):
     if (int(X/n)):
         return NsinFrom10sin(int(X/n), n)+str(X%n)
     return str(X%n)
 
 def main():
+  cnt = 0
   #input N
-  print('input N in the range 0<=N<=30')
+  #print('input N in the range 0<=N<=30')
   N = int(input())
   if N>30:
       pass
   else:
       N = 4**N
-  #list01 = range(1,N)
-      list01 = [ list(str(NsinFrom10sin(x,4))) for x in range(1,N) ]
-      print(list01)
-      list01 = reduce(lambda x, y: x + y, list01)
-      print(list01)
-      print(list01.count('3'))
+      list01 = [ NsinFrom10sin(x,4) for x in range(1,N+1) ]
+      #print(list01)
+      #print(type(len(list01)))
+      for i in list01:
+          tmp = i.count('3')
+          cnt += tmp
+      print(cnt)
 
 if __name__ == "__main__":
   main()
