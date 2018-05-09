83d4a9add7de2e38451107e4dea3b889424bb769
mtgorori <karu7otit2heki39@gmail.com>
2018-05-09 17:05:23 +0900

    一旦，文字列時代に戻す． 一つずつ取り出して処理する方法ではなかなかスピードが出ないようだ

--- challengeProblemA.py
+++ challengeProblemA.py
@@ -4,6 +4,8 @@ import pstats
 from memory_profiler import profile
 from numba import jit
 
+
+from functools import reduce
 @jit
 def NsinFrom10sin(X, n):
     if (int(X/n)):
@@ -12,23 +14,22 @@ def NsinFrom10sin(X, n):
 
 @jit
 def main():
+
   cnt = 0
   tmp = 0
+
   #input N
-  #print('input N in the range 0<=N<=30')
+  print('input N in the range 0<=N<=30')
   N = int(input())
   if N>30:
       pass
   else:
       N = 4**N
-      for i in range(1,N+1):
-          x = NsinFrom10sin(i, 4)
-          tmp = x.count('3')
-          cnt += tmp
-      print(cnt)
+  #list01 = range(1,N)
+      list01 = [ list(str(NsinFrom10sin(x,4))) for x in range(1,N) ]
+      print(list01)
+      list01 = reduce(lambda x, y: x + y, list01)
+      print(list01.count('3'))
 
 if __name__ == "__main__":
     main()
- # cProfile.run('main()', filename = 'main.prof')
-  #sts = pstats.Stats('main.prof')
-  #sts.strip_dirs().sort_stats('cumulative').print_stats()
