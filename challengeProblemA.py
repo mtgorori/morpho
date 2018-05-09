eab5b4b865f97fc0ac9cdba9a50f8cada2a14602
mtgorori <karu7otit2heki39@gmail.com>
2018-05-09 15:34:22 +0900

    nnumpyを導入

--- challengeProblemA.py
+++ challengeProblemA.py
@@ -2,10 +2,11 @@ import sys
 import cProfile
 import pstats
 from memory_profiler import profile
+import  numpy as np
 
 def NsinFrom10sin(X, n):
     if (int(X/n)):
-        return NsinFrom10sin(int(X/n), n)+str(X%n)
+        return NsinFrom10sin(int(X/n), n)+ str(X%n)
     return str(X%n)
 
 @profile
@@ -23,7 +24,6 @@ def main():
           tmp = x.count('3')
           cnt += tmp
       print(cnt)
-      print(sys.getsizeof(list01))
 
 if __name__ == "__main__":
   cProfile.run('main()', filename = 'main.prof')
