bc441c086264e074fe281e73dd71f812d0a4edac
mtgorori <karu7otit2heki39@gmail.com>
2018-05-09 14:56:54 +0900

    メモリプロファイルを行うようにした． メモリが乗らない状態にある．

--- challengeProblemA.py
+++ challengeProblemA.py
@@ -1,8 +1,14 @@
+import sys
+import cProfile
+import pstats
+from memory_profiler import profile
+
 def NsinFrom10sin(X, n):
     if (int(X/n)):
         return NsinFrom10sin(int(X/n), n)+str(X%n)
     return str(X%n)
 
+@profile
 def main():
   cnt = 0
   #input N
@@ -19,6 +25,9 @@ def main():
           tmp = i.count('3')
           cnt += tmp
       print(cnt)
+      print(sys.getsizeof(list01))
 
 if __name__ == "__main__":
-  main()
+  cProfile.run('main()', filename = 'main.prof')
+  sts = pstats.Stats('main.prof')
+  sts.strip_dirs().sort_stats('cumulative').print_stats()
