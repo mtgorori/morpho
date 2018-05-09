36ff1c65c3241e86f8c4ff7f45e69064ab40b3eb
mtgorori <karu7otit2heki39@gmail.com>
2018-05-09 16:47:28 +0900

    初期化等細かい調整

--- challengeProblemA.py
+++ challengeProblemA.py
@@ -2,16 +2,18 @@ import sys
 import cProfile
 import pstats
 from memory_profiler import profile
-import  numpy as np
+from numba import jit
 
+@jit
 def NsinFrom10sin(X, n):
     if (int(X/n)):
         return NsinFrom10sin(int(X/n), n)+ str(X%n)
     return str(X%n)
 
-@profile
+@jit
 def main():
   cnt = 0
+  tmp = 0
   #input N
   #print('input N in the range 0<=N<=30')
   N = int(input())
@@ -26,6 +28,7 @@ def main():
       print(cnt)
 
 if __name__ == "__main__":
-  cProfile.run('main()', filename = 'main.prof')
-  sts = pstats.Stats('main.prof')
-  sts.strip_dirs().sort_stats('cumulative').print_stats()
+    main()
+ # cProfile.run('main()', filename = 'main.prof')
+  #sts = pstats.Stats('main.prof')
+  #sts.strip_dirs().sort_stats('cumulative').print_stats()
