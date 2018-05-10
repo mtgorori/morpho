1f509ce5fae3202a582595a55ff60b706ccf119b
mtgorori <karu7otit2heki39@gmail.com>
2018-05-10 17:41:34 +0900

    メモ化関数を入れたが効果なし

--- challengeProblemA.py
+++ challengeProblemA.py
@@ -1,33 +1,35 @@
-import sys
-import cProfile
-import pstats
-from memory_profiler import profile
-from numba import jit
-from functools import reduce
+import numba
 
-def NsinFrom10sin(X, n):
-    if (int(X/n)):
-        return NsinFrom10sin(int(X/n), n)+ str(X%n)
-    return str(X%n)
+def memoize(f): #メモ化関数
+    table = {}
+    def func(*args):
+        if not args in table:
+            table[args] = f(*args)
+        return table[args]
+    return func
 
-def main():
+@memoize
+def FoursinFrom10sin(X):
+    if (int(X/4)):
+        return FoursinFrom10sin(int(X/4))+ str(X%4)
+    return str(X%4)
+
+@numba.jit
+def count():
   cnt = 0
   tmp = 0
-  #input N
-  #print('input N in the range 0<=N<=30')
   N = int(input())
   if N>30:
       pass
   else:
       N = 4**N
-  #list01 = range(1,N)
-      list01 = [ list(str(NsinFrom10sin(x,4))) for x in range(1,int(N/2)+1) ]
-      list01 = reduce(lambda x, y: x + y, list01)
-      list02 = [ list(str(NsinFrom10sin(x,4))) for x in range(int(N/2)+1,N+1) ]
-      list02 = reduce(lambda x, y: x + y, list02)
-      print(list01)
-      print(list02)
-      print(list01.count('3')+list02.count('3'))
+      list01 = []
+      for x in range(1,N+1):
+          list01.append(FoursinFrom10sin(x))
+      for i in list01:
+          tmp = i.count('3')
+          cnt += tmp
+      print(cnt)
 
 if __name__ == "__main__":
-    main()
+    count()
