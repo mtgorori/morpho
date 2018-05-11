03dc82af3d1eb33673e324fa0c15ee18df0c263a
mtgorori <33616505+mtgorori@users.noreply.github.com>
2018-05-11 10:55:41 +0900

    同じ

--- challengeProblemA.py
+++ challengeProblemA.py
@@ -2,17 +2,24 @@ import sys
 import cProfile
 import pstats
 from memory_profiler import profile
+<<<<<<< HEAD
 from numba import jit
 from functools import reduce
+=======
+import  numpy as np
+>>>>>>> parent of 36ff1c6... 初期化等細かい調整
 
 def NsinFrom10sin(X, n):
     if (int(X/n)):
         return NsinFrom10sin(int(X/n), n)+ str(X%n)
     return str(X%n)
 
+<<<<<<< HEAD
+=======
+@profile
+>>>>>>> parent of 36ff1c6... 初期化等細かい調整
 def main():
   cnt = 0
-  tmp = 0
   #input N
   #print('input N in the range 0<=N<=30')
   N = int(input())
@@ -30,4 +37,10 @@ def main():
       print(list01.count('3')+list02.count('3'))
 
 if __name__ == "__main__":
+<<<<<<< HEAD
     main()
+=======
+  cProfile.run('main()', filename = 'main.prof')
+  sts = pstats.Stats('main.prof')
+  sts.strip_dirs().sort_stats('cumulative').print_stats()
+>>>>>>> parent of 36ff1c6... 初期化等細かい調整
