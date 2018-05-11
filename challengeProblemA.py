0d71991e000fe4310503c74124a8d0ab8b325a85
mtgorori <33616505+mtgorori@users.noreply.github.com>
2018-05-11 11:15:30 +0900

    リストを省いた．

--- challengeProblemA.py
+++ challengeProblemA.py
@@ -2,30 +2,9 @@ import sys
 import cProfile
 import pstats
 from memory_profiler import profile
-<<<<<<< HEAD
-<<<<<<< HEAD
-from numba import jit
-from functools import reduce
-=======
-import  numpy as np
->>>>>>> parent of 36ff1c6... 初期化等細かい調整
-=======
-import  numpy as np
->>>>>>> parent of 36ff1c6... 初期化等細かい調整
+import numpy as np
 
-def NsinFrom10sin(X, n):
-    if (int(X/n)):
-        return NsinFrom10sin(int(X/n), n)+ str(X%n)
-    return str(X%n)
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 @profile
->>>>>>> parent of 36ff1c6... 初期化等細かい調整
-=======
-@profile
->>>>>>> parent of 36ff1c6... 初期化等細かい調整
 def main():
   cnt = 0
   #input N
@@ -35,26 +14,14 @@ def main():
       pass
   else:
       N = 4**N
-  #list01 = range(1,N)
-      list01 = [ list(str(NsinFrom10sin(x,4))) for x in range(1,int(N/2)+1) ]
-      list01 = reduce(lambda x, y: x + y, list01)
-      list02 = [ list(str(NsinFrom10sin(x,4))) for x in range(int(N/2)+1,N+1) ]
-      list02 = reduce(lambda x, y: x + y, list02)
-      print(list01)
-      print(list02)
-      print(list01.count('3')+list02.count('3'))
+      for i in range(1,N+1):
+          tmp = np.base_repr(i,4)
+          tmp = tmp.count('3')
+          cnt += tmp
+      print(cnt)
+      print(sys.getsizeof(list01))
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-<<<<<<< HEAD
-    main()
-=======
   cProfile.run('main()', filename = 'main.prof')
   sts = pstats.Stats('main.prof')
   sts.strip_dirs().sort_stats('cumulative').print_stats()
->>>>>>> parent of 36ff1c6... 初期化等細かい調整
-=======
-  cProfile.run('main()', filename = 'main.prof')
-  sts = pstats.Stats('main.prof')
-  sts.strip_dirs().sort_stats('cumulative').print_stats()
->>>>>>> parent of 36ff1c6... 初期化等細かい調整
