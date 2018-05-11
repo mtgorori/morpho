054b59957833985e280cb6c82fa9a230b2d1eaa0
mtgorori <karu7otit2heki39@gmail.com>
2018-05-11 12:46:46 +0900

    基数変換関数を導入した

--- challengeProblemA.py
+++ challengeProblemA.py
@@ -1,21 +1,8 @@
 import numba
-
-def memoize(f): #メモ化関数
-    table = {}
-    def func(*args):
-        if not args in table:
-            table[args] = f(*args)
-        return table[args]
-    return func
-
-@memoize
-def FoursinFrom10sin(X):
-    if (int(X/4)):
-        return FoursinFrom10sin(int(X/4))+ str(X%4)
-    return str(X%4)
+import numpy as np
 
 @numba.jit
-def count():
+def main():
   cnt = 0
   tmp = 0
   N = int(input())
@@ -25,11 +12,11 @@ def count():
       N = 4**N
       list01 = []
       for x in range(1,N+1):
-          list01.append(FoursinFrom10sin(x))
+          list01.append(np.base_repr(x,4))
       for i in list01:
           tmp = i.count('3')
           cnt += tmp
       print(cnt)
 
 if __name__ == "__main__":
-    count()
+    main()
