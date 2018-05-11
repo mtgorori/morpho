c55a7c387113b9688916fffd411472eb2d09b689
mtgorori <karu7otit2heki39@gmail.com>
2018-05-11 13:29:06 +0900

    変数の無駄を省いた．

--- challengeProblemA.py
+++ challengeProblemA.py
@@ -5,15 +5,12 @@ from memory_profiler import profile
 import numpy as np
 from numba import jit
 
-
-
 @profile
 @jit
 def cnt3():
     digits = '0123'
     cnt = 0
     tmp = 0
-    tmp1 = 0
     #input N
     #print('input N in the range 0<=N<=30')
     N = int(input())
@@ -23,14 +20,12 @@ def cnt3():
         N = 4**N
         #change to xrange if python2x
         for i in range(1,N+1):
-            num = abs(i)
+            num = i
             res = []
             while num:
                 res.append(digits[num % 4])
                 num //= 4
-            tmp1 = ''.join(reversed(res or '0'))
-            tmp = tmp1.count('3')
-            cnt += tmp
+            cnt += ''.join(reversed(res or '0')).count('3')
     print(cnt)
 
 if __name__ == "__main__":
